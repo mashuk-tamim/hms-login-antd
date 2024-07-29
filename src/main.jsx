4e7f0cf94c1a2ecf240d792dfc5806ebe7bd54df
mashuk-tamim <tamimmashuk328@gmail.com>
2024-07-29 15:59:01 +0600

    import alias @ is implemented in the codebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Welcome from "./welcome.jsx";
+import Welcome from "@/welcome.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
